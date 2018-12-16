b4cdf4a66b32b1b287b29470a0602a8853a057cf
ming71 <43720092+ming71@users.noreply.github.com>
2018-12-16 22:21:20 +0800

    Update img_format_trans.py

--- img_format_trans.py
+++ img_format_trans.py
@@ -1,3 +1,7 @@
+'''
+replace the raw images with aimed format images
+'''
+
 import os
 import cv2
 import sys
@@ -10,4 +14,13 @@ for filename in os.listdir(path):
     if os.path.splitext(filename)[1] == '.jpg':
         img = Image.open(path+"/"+filename)
         img.save(path + '/' +  filename)
+'''     
+def bmpToJpg(file_path):
+    for fileName in os.listdir(file_path):
+        # print(fileName)
+        newFileName = fileName[0:fileName.find("_")]+".jpg"
+        print(newFileName)
+        im = Image.open(file_path+"\\"+fileName)
+        im.save(file_path+"\\"+newFileName)
+'''
 
